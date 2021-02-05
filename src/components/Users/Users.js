2e617ac61823dcbc4b64b75865cf922844332c05
Usama Sadiq <usamasadiq029@gmail.com>
2021-02-05 12:26:11 +0500

    View Users Work Completed

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+
 const Users = () => {
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    website: "",
+  });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const result = await axios.get(`http://localhost:3001/users/${id}`);
+      setUser(result.data);
+    };
+
+    loadUsers();
+  }, [id]);
+
+  const { name, username, email, phone, website } = user;
+
   return (
-    <div>
-      <h1>Users</h1>
+    <div className="container py-4">
+      <h1 className="display-4">User: {id} Details</h1>
+      <hr />
+      <ul className="list-group w-50">
+        <li className="list-group-item">Name: {name}</li>
+        <li className="list-group-item">Username: {username}</li>
+        <li className="list-group-item">Email: {email}</li>
+        <li className="list-group-item">Phone: {phone}</li>
+        <li className="list-group-item">Website: {website}</li>
+      </ul>
+      <Link className="btn btn-primary mt-4" to="/">
+        Back
+      </Link>
     </div>
   );
 };
